e0b66203951b88c071f489e86bb57fd4597979ff
Derek Helm <derekred9@gmail.com>
2016-08-16 21:20:25 -0400

    added Mrs. Buckey to Java Classes

---
+++ FtcRobotController/src/main/java/com/powerstackers/javaclasses/buckey/opmodes/autonomous/BuckeyAutoDrive1.java
@@ -0,0 +1,7 @@
+package com.powerstackers.javaclasses.buckey.opmodes.autonomous;
+
+/**
+ * Created by root on 8/16/16.
+ */
+public class BuckeyAutoDrive1 {
+}
---
+++ FtcRobotController/src/main/java/com/powerstackers/javaclasses/buckey/opmodes/teleop/BuckeyTeleDrive1.java
@@ -0,0 +1,7 @@
+package com.powerstackers.javaclasses.buckey.opmodes.teleop;
+
+/**
+ * Created by root on 8/16/16.
+ */
+public class BuckeyTeleDrive1 {
+}
