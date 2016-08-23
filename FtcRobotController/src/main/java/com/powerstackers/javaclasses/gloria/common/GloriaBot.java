4fd0ad929b3d93ed314b901a6bebd26a6daa4dae
Derek Helm <derekred9@gmail.com>
2016-08-23 19:41:42 -0400

    added gloria

---
+++ FtcRobotController/src/main/java/com/powerstackers/javaclasses/gloria/common/GloriaBot.java
@@ -0,0 +1,7 @@
+package com.powerstackers.javaclasses.gloria.common;
+
+/**
+ * Created by root on 8/23/16.
+ */
+public class GloriaBot {
+}
---
+++ FtcRobotController/src/main/java/com/powerstackers/javaclasses/gloria/opmodes/autonomous/GloriaAutoDrive1.java
@@ -0,0 +1,7 @@
+package com.powerstackers.javaclasses.gloria.opmodes.autonomous;
+
+/**
+ * Created by root on 8/23/16.
+ */
+public class GloriaAutoDrive1 {
+}
