21f92b89e8943c79a1690b3a68783e0d5ac66574
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2016-10-08 16:00:11 -0400

    Adds basic motor definitions
    We're starting to implement Mecanum drive. We've got the empty method there, and the motor definitions, but not much else.

--- TeamCode/src/main/java/com/powerstackers/velocity/common/VelRobot.java
+++ TeamCode/src/main/java/com/powerstackers/velocity/common/VelRobot.java
@@ -1,6 +1,7 @@
 package com.powerstackers.velocity.common;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * @author Derek Helm
@@ -9,13 +10,30 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class VelRobot {
 
 
+    /*
+    Looking at the robot from above:
+        -------------
+        |1         2|
+        |           |
+        |           |
+        |3         4|
+        -------------
+     */
+    DcMotor drive1;
+    DcMotor drive2;
+    DcMotor drive3;
+    DcMotor drive4;
+
+
     /**
      * Construct a Robot object.
      * @param mode The OpMode in which the robot is being used.
      */
     public VelRobot(OpMode mode) {
-
-        //methods for main class here vvv
+        drive1 = mode.hardwareMap.dcMotor.get("motorFrontLeft");
+        drive2 = mode.hardwareMap.dcMotor.get("motorFrontRight");
+        drive3 = mode.hardwareMap.dcMotor.get("motorBackLeft");
+        drive4 = mode.hardwareMap.dcMotor.get("motorBackRight");
 
     }
 
@@ -23,8 +41,18 @@ public class VelRobot {
      * Initialize the robot's servos and sensors.
      */
     public void initializeRobot() /*throws InterruptedException */{
+        // TODO set motor modes
+        stopMovement();
+    }
 
-        //init code here vvv
+    public void setMovement(double angle, double speed, double rotation) {
+        
+    }
 
+    public void stopMovement() {
+        drive1.setPower(0.0);
+        drive2.setPower(0.0);
+        drive3.setPower(0.0);
+        drive4.setPower(0.0);
     }
 }
