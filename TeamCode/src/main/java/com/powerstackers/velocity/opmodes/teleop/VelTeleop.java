fdf2f212a5e44590448527e09ee28f7d7e3c88b6
Derek Helm <derekred9@gmail.com>
2016-10-27 14:37:28 -0400

    added joystick methods for mec drive

--- TeamCode/src/main/java/com/powerstackers/velocity/opmodes/teleop/VelTeleop.java
+++ TeamCode/src/main/java/com/powerstackers/velocity/opmodes/teleop/VelTeleop.java
@@ -5,6 +5,10 @@ import com.powerstackers.velocity.common.enums.PublicEnums.AllianceColor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.abs;
 
 /**
  * @author Derek Helm
@@ -12,13 +16,23 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="VEL-Teleop", group ="Powerstackers")
 public class VelTeleop extends OpMode {
+
     private ElapsedTime runtime = new ElapsedTime();
+
+    private static final float MINIMUM_JOYSTICK_THRESHOLD = 0.15F;
 
     //constructors
     AllianceColor allianceColor;
     VelRobot robot;
 
     //declarations here vvv
+    float stickMoveUpDown;
+//    float stickMoveDown;
+//    float stickMoveRight;
+    float stickMoveLeftRight;
+    float stickTurnRight;
+    float stickTurnLeftRight;
+
     boolean buttonVexMotorForward;
     boolean buttonVexMotorBackward;
 
@@ -69,6 +83,14 @@ public class VelTeleop extends OpMode {
     public void loop() {
         telemetry.addData("Status", "Running: ");
 
+        // Read the joystick and determine what motor setting to use.
+        stickMoveUpDown    = (float) scaleInput(Range.clip(-gamepad1.left_stick_y, -1, 1));
+//        stickMoveDown  = (float) scaleInput(Range.clip(+gamepad1 .left_stick_y, -1, 1));
+//        stickMoveRight = (float) scaleInput(Range.clip(+gamepad1.left_stick_x, -1, 1));
+        stickMoveLeftRight  = (float) scaleInput(Range.clip(-gamepad1.left_stick_x, -1, 1));
+//        stickTurnRight = (float) scaleInput(Range.clip(+gamepad1.right_stick_x, -1, 1));
+        stickTurnLeftRight  = (float) scaleInput(Range.clip(-gamepad1.right_stick_x, -1, 1));
+
         //button maps here vvv
         buttonVexMotorForward  = gamepad1.dpad_up;
         buttonVexMotorBackward = gamepad1.dpad_down;
@@ -80,6 +102,18 @@ public class VelTeleop extends OpMode {
             robot.vexPower(-1);
         } else {
             robot.vexPower(0);
+        }
+
+        // TODO: 10/27/16 add get stickTurnLeftRight Value -=left +=right
+        
+        if (abs(stickMoveUpDown) > MINIMUM_JOYSTICK_THRESHOLD) {
+            robot.setMovement((PI/2), -stickMoveUpDown, 0);
+        } else if (abs(stickMoveLeftRight) > MINIMUM_JOYSTICK_THRESHOLD) {
+            robot.setMovement(((3*PI)/2), -stickMoveLeftRight, 0);
+        } else if (abs(stickTurnLeftRight) > MINIMUM_JOYSTICK_THRESHOLD) {
+            robot.setMovement((PI/2), -stickTurnLeftRight, -stickTurnLeftRight);
+        } else {
+                robot.stopMovement();
         }
 
 //        telemetry here vvv
@@ -95,5 +129,39 @@ public class VelTeleop extends OpMode {
         //stop code here vvv
         robot.stopMovement();
 
+    }
+
+    /**
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
 }
