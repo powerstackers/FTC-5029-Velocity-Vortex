2ee5e326aa1afe200683d282d7acb92668911536
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2016-11-22 15:17:27 -0500

    Removes JonsAlgo
    Moved all the functions from JonsAlgo into their proper place in the VelRobotAuto class.

--- TeamCode/src/main/java/com/powerstackers/velocity/common/VelRobotAuto.java
+++ TeamCode/src/main/java/com/powerstackers/velocity/common/VelRobotAuto.java
@@ -20,9 +20,12 @@
 
 package com.powerstackers.velocity.common;
 
+import com.powerstackers.velocity.common.enums.PublicEnums;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.Range;
 
 import static com.powerstackers.velocity.common.VelRobotConstants.BEACON_RESTING;
+import static java.lang.Math.PI;
 
 /**
  * Basic configurations for our robot in autonomous mode. All the functionality of a teleop bot,
@@ -33,7 +36,18 @@ import static com.powerstackers.velocity.common.VelRobotConstants.BEACON_RESTING
 
 public class VelRobotAuto extends VelRobot {
 
-    public VelJonsAlgo algorithm;
+
+    /**
+     * Stores the number of encoder ticks in one motor revolution.
+     * For AndyMark Neverest 40's, it's 280. The encoder tick count is actually 7 pulses per
+     * revolution. Since the gearbox increases the number of rotations by a factor of 40, the final
+     * count is 7 * 40 = 280. For 20 or 60 reduction motors, the  number would be different.
+     */
+    static double ticksPerRevolution = 1120; // Number of encoder ticks per motor rotation
+    static double wheelDiameter = 4;         // Diameter of your wheels in inches
+    static double driveGearMultiplier = 1.0; // Drive gear multiplier.
+    // EXAMPLE: If your drive train is geared 2:1 (1 motor rotation = 2 wheel rotations), set this to 2
+//    double turnOvershootThreshold = 0.1;
 
     /**
      * Construct a Robot object.
@@ -43,7 +57,6 @@ public class VelRobotAuto extends VelRobot {
 
         super(mode);
         //constructors
-        algorithm = new VelJonsAlgo(this);
     }
 
     /**
@@ -70,8 +83,111 @@ public class VelRobotAuto extends VelRobot {
 
     }
 
+    /**
+     * Tap the beacon on the correct side.
+     * @param allianceColor The color that we are currently playing as.
+     */
+    public void tapBeacon(PublicEnums.AllianceColor allianceColor) {
+        PublicEnums.AllianceColor dominantColor;
+        double positionBeaconServo;
+
+        // Detect the color shown on the beacon's left half, and record it.
+        if (sensorColor.red() > sensorColor.blue()) {
+            dominantColor = PublicEnums.AllianceColor.RED;
+        } else {
+            dominantColor = PublicEnums.AllianceColor.BLUE;
+        }
+
+        // Tap the correct side based on the dominant color.
+        if (dominantColor == allianceColor) {
+            positionBeaconServo = VelRobotConstants.BEACON_TAP_LEFT;
+        } else {
+            positionBeaconServo = VelRobotConstants.BEACON_TAP_RIGHT;
+        }
+
+        // Trim the servo value and set the servo position.
+        positionBeaconServo = trimServoValue(positionBeaconServo);
+        servoBeacon.setPosition(positionBeaconServo);
+    }
+
+    /**
+     * Turn the robot a certain number of degrees from center.
+     * @param degrees Number of DEGREES to turn. Positive is counterclockwise, negative is clockwise.
+     * @param speed Speed at which to turn.
+     * @throws InterruptedException Make sure that we don't get trapped in this method when interrupted.
+     */
+    void turnDegrees(double degrees, double speed) throws InterruptedException {
+
+        double degreesSoFar = getGyroHeading();
+
+        if (degrees > 180) {
+//            robot.setPowerLeft(-1 * speed);
+//            robot.setPowerRight(speed);
+            mode.telemetry.addData("gyro1", getGyroHeading());
+        } else if (degrees < 180 || degrees == 180) {
+//            robot.setPowerLeft(speed);
+//            robot.setPowerRight(-1 * speed);
+            mode.telemetry.addData("gyro2", getGyroHeading());
+        } else {
+//            robot.setPowerAll(0);
+        }
+        mode.telemetry.addData("Gyro", degrees + "," + degreesSoFar);
+        // For as long as the current degree measure doesn't equal the target. This will work in the clockwise and
+        // counterclockwise directions, since we are comparing the absolute values
+        while ((degreesSoFar) < (degrees)) {
+            mode.telemetry.addData("gyrocompare", degreesSoFar=getGyroHeading());
+        }
+
+        // Stop all drive motors
+//        robot.setPowerAll(0);
+    }
+
+    /**
+     * Move the robot across the playing field a certain distance.
+     * Indicating a negative speed or distance will cause the robot to move in reverse.
+     * @param  distance The distance that we want to travel, in centimeters.
+     * @param angle The angle to move at, in radians.
+     * @param  speed The speed at which to travel.
+     */
+    public void goDistanceInCm(double distance, double angle, double speed) {
+        // TODO Figure out how to do this.
+    }
+
+    /**
+     * Converts a distance in encoder ticks to a distance in inches.
+     * <p>We calculate this by taking the number of ticks traveled, divided by the number of ticks
+     * per revolution, and then multiplied by the gear ratio multiplier to get the number of wheel
+     * rotations. Multiply one more time by the circumference of the wheels (PI*wheelDiameter).
+     * @param  ticks long representing the distance in ticks.
+     * @return       that distance in inches.
+     */
+    public static double ticksToCm(long ticks) {
+        // TODO This is wrong.
+        return (ticks/ticksPerRevolution)*driveGearMultiplier*(PI*wheelDiameter);
+    }
+
+    /**
+     * Converts a distance in inches to a distance in encoder ticks.
+     * <p>We calculate this by taking the number of wheel rotations (inches/(PI*wheelDiameter))
+     * multiplied by the inverse of the gear ratio, to get the number of motor rotations. Multiply
+     * one more time by the number of motor encoder ticks per one motor revolution.
+     * @param  inches double containing the distance you want to travel.
+     * @return        that distance in encoder ticks.
+     */
+    public static double cmToTicks(double inches) {
+        // TODO This is wrong now.
+        return (long) ((1/driveGearMultiplier)*ticksPerRevolution*(inches/(PI*wheelDiameter)));
+    }
 
 
+    /**
+     * Trim a servo value between the minimum and maximum ranges.
+     * @param servoValue Value to trim.
+     * @return A raw double with the trimmed value.
+     */
+    private static double trimServoValue(double servoValue) {
+        return Range.clip(servoValue, 0.0, 1.0);
+    }
 
     public long getLeftEncoder() {
         return motorDrive1.getCurrentPosition();
