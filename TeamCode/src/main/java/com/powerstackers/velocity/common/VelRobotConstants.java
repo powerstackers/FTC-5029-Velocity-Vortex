f3b7d3988952047e10f1f5fded5f9daaef6621c0
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2016-11-22 14:40:11 -0500

    Fixes robot constants indentation
    Two tabs? Srsly?

--- TeamCode/src/main/java/com/powerstackers/velocity/common/VelRobotConstants.java
+++ TeamCode/src/main/java/com/powerstackers/velocity/common/VelRobotConstants.java
@@ -5,19 +5,16 @@ package com.powerstackers.velocity.common;
  */
 
 public class VelRobotConstants {
-        // Servo settings
-        public static final double BEACON_TAP_LEFT  = 0.8;
-        public static final double BEACON_TAP_RIGHT = 0.2;
-        public static final double BEACON_RESTING   = 0.5;
-        public static final double TRIMM_MOTOR         = 0.88888888888;
+    // Servo settings
+    public static final double BEACON_TAP_LEFT  = 0.8;
+    public static final double BEACON_TAP_RIGHT = 0.2;
+    public static final double BEACON_RESTING   = 0.5;
+    public static final double TRIMM_MOTOR         = 0.88888888888;
 
-        public static final double MINIMUM_JOYSTICK_THRESHOLD = 0.15F;
+    public static final double MINIMUM_JOYSTICK_THRESHOLD = 0.15F;
 
-        public static final double SERVO_ARMS_RELEASE_RESTING = 0.0;
-        public static final double SERVO_ARMS_RELEASE_OPEN = 0.0;
-        public static final double SERVO_ARMS_GRAB_TIGHT = 0.0;
-        public static final double SERVO_ARMS_GRAB_LOOSE = 0.0;
-
-        // Motor speeds
-
+    public static final double SERVO_ARMS_RELEASE_RESTING = 0.0;
+    public static final double SERVO_ARMS_RELEASE_OPEN = 0.0;
+    public static final double SERVO_ARMS_GRAB_TIGHT = 0.0;
+    public static final double SERVO_ARMS_GRAB_LOOSE = 0.0;
 }
